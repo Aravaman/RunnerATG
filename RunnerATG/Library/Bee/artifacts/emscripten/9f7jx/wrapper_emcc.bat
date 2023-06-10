3ff0daea67ec1b565740c84e3e0d3a4d1c1e9bf8
Авагян Арман <69074875+Aravaman@users.noreply.github.com>
2023-06-10 19:27:22 +0500

    Up2

--- RunnerATG/Library/Bee/artifacts/emscripten/9f7jx/wrapper_emcc.bat
+++
@@ -1,4 +0,0 @@
-
-@echo off
-"D:\Program File\Unity\File\I\2021.3.17f1\Editor\Data\PlaybackEngines\WebGLSupport\BuildTools\Emscripten\emscripten\emcc.bat" %* < nul
-exit %ERRORLEVEL%
--- RunnerATG/Library/Bee/artifacts/emscripten/uryhn/wrapper_emar.bat
+++
@@ -1,4 +0,0 @@
-
-@echo off
-"D:\Program File\Unity\File\I\2021.3.17f1\Editor\Data\PlaybackEngines\WebGLSupport\BuildTools\Emscripten\emscripten\emar.bat" %* < nul
-exit %ERRORLEVEL%
